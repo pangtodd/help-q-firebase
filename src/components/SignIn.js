6038a4ebfc362fcee2ce0969df0a2b555fd56c29
Todd Pangilinan <pang.todd@gmail.com>
2022-08-30 17:26:28 -0700

    established doSignIn function, and updated return so there is a form for signIn.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { auth } from "./../firebase.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailandPassword } from "firebase/auth";
 
 function SignIn(){
   const [signUpSuccess, setSignUpSuccess] =useState(null);
+  const [signInSuccess, setSignInSuccess] = useState(null);
 
   function doSignUp(event){
     event.preventDefault();
@@ -18,6 +19,19 @@ function SignIn(){
       });
   }
 
+  function doSignIn(event){
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password= event.target.signinPassword.value;
+    signInWithEmailandPassword(auth, email, password)
+      .then((userCredential)=>{
+        setSignInSuccess(`You've done it! You're signed in as ${userCredential.user.email}!`)
+      })
+      .catch((error)=> {
+        setSignInSuccess(`There was an error signing in: ${error.message}!`)
+      });
+  }
+
   return (
     <React.Fragment>
     <h1>Sign up</h1>
@@ -27,10 +41,33 @@ function SignIn(){
           type='text'
           name='email'
           placeholder='email' />
+          <br></br>
+          <br></br>
         <input
           type='password'
           name='password'
           placeholder='Password' />
+          <br></br>
+          <br></br>
+          <button type='submit'>Submit!</button>
+      </form>
+
+      <h1>Sign in</h1>
+      {signInSuccess}
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+          <br></br>
+          <br></br>
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+          <br></br>
+          <br></br>
+          <button type='submit'>Submit!</button>
       </form>
     </React.Fragment>
   );
