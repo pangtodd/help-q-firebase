ecd55311b389d80f8e1a4c4d1d4523be457369f7
Todd Pangilinan <pang.todd@gmail.com>
2022-08-30 17:07:38 -0700

    bugfix, had 'export defualt', didn't need the 'default'.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { auth } from "./../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignIn(){
-  
+  function doSignUp(event){
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential)=>{
+        //TBD
+      })
+      .catch((error)=>{
+        //TBD
+      });
+  }
   return (
     <React.Fragment>
     <h1>Sign up</h1>
