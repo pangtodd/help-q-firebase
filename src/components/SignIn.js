202bc11b676b52eb09c1172a53f0bbb852e9c305
Todd Pangilinan <pang.todd@gmail.com>
2022-08-30 17:08:43 -0700

    updated SignIn.js to accept authentication.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,23 +1,27 @@
-import React from "react";
-import { auth } from "./../firebase";
+import React, { useState } from "react";
+import { auth } from "./../firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignIn(){
+  const [signUpSuccess, setSignUpSuccess] =useState(null);
+
   function doSignUp(event){
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential)=>{
-        //TBD
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
       })
       .catch((error)=>{
-        //TBD
+        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
       });
   }
+
   return (
     <React.Fragment>
     <h1>Sign up</h1>
+    {signUpSuccess}
       <form onSubmit={doSignUp}>
         <input
           type='text'
